ef815f4793a173ffb00cfc4cf80d49b42ed64bb3
chenzengit <ikhoata@gmail.com>
2020-05-04 00:08:58 +0700

    asd

--- cs.js
+++
@@ -1,4 +0,0 @@
-window.timerOW = 7000;
-window.url_js_gb = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.0/gb.js";
-window.url_js_nor = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.0/nor.js";
-window.url_js_main = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.0/main.js";

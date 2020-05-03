e134839e47781d4ba6e4e52d55ed50776d87a5a9
chenzengit <ikhoata@gmail.com>
2020-05-04 01:13:44 +0700

    Create cs.js

---
+++ cs.js
@@ -0,0 +1,21 @@
+window.timerOW = 7000;
+
+window.url_js_gb = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/gb.js";
+window.url_js_nor = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/nor.js";
+window.url_js_main = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/main.js";
+
+// window.url_js_gb = "../gb.js";
+// window.url_js_nor = "../nor.js";
+// window.url_js_main = "../main.js";
+
+var script = document.createElement('script'); 
+script.src = url_js_gb;
+document.head.appendChild(script);
+
+var script = document.createElement('script'); 
+script.src = url_js_nor;
+document.head.appendChild(script);
+
+var script = document.createElement('script'); 
+script.src = url_js_main;
+document.head.appendChild(script);

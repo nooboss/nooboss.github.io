b169adc6c0c52e4c13a47eb942229bdc5c4f2691
chenzengit <ikhoata@gmail.com>
2020-05-04 01:15:46 +0700

    sdf

--- cs_kiki.js
+++ cs_kiki.js
@@ -1,8 +1,8 @@
 window.timerOW = 7000;
 
-window.url_js_gb = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/gb.js";
-window.url_js_nor = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/nor.js";
-window.url_js_main = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.1/main.js";
+window.url_js_gb = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.2/gb.js";
+window.url_js_nor = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.2/nor.js";
+window.url_js_main = "https://cdn.jsdelivr.net/gh/nooboss/noob_libs@1.0.2/main.js";
 
 // window.url_js_gb = "../gb.js";
 // window.url_js_nor = "../nor.js";
